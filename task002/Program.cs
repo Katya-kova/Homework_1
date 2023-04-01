d6e6560190e91bfe9bfa329664e248bde1bc3047
Katerina <k_by96@mail.ru>
2023-04-02 01:09:30 +0300

    Task003 added

---
+++ task002/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Write a namber a, b and c: ");
+int a, b, c;
+if (int.TryParse (Console.ReadLine(), out a) && int.TryParse (Console.ReadLine(), out b) && int.TryParse (Console.ReadLine (), out c))
+    
+    if (a > b)
+    {
+        if ( a > c)
+        {
+        Console.Write("max: ");
+        Console.Write(a);
+        }
+        else 
+        {
+            if ( b > c )
+            {
+                Console.Write("max: ");
+                Console.Write(b);
+            }
+            else 
+            {
+                Console.Write("max: ");
+                Console.Write(c);
+            }
+         }
+
+    }
+
+    else 
+    {
+        Console.Write("max: ");
+        Console.WriteLine(c);
+    }
+
+else 
+{
+    Console.WriteLine ("It's not a number");
+}
+
