c5dba2046eebff331bf5e662a3316d493aea55b3
Katerina <k_by96@mail.ru>
2023-04-01 23:05:18 +0300

    Gitignore added

---
+++ task001/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Write a namber a and b: ");
+int a;
+int b;
+if (int.TryParse (Console.ReadLine(), out a) && int.TryParse (Console.ReadLine(), out b))
+    
+    if (a > b)
+    {
+        Console.Write("max: ");
+        Console.Write(a);
+        Console.Write("min: ");
+        Console.Write(b);
+    }
+    else 
+    {
+        Console.Write("max: ");
+        Console.WriteLine(b);
+        Console.Write("min: ");
+        Console.Write(a);
+    }
+
+else 
+{
+    Console.WriteLine ("It's not a number");
+}
